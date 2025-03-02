a91c47642c96d12b44a7ff18e168d384b9f88616
VaradRane12 <varadjrane@gmail.com>
2025-03-02 23:28:28 +0530

    added stock adding and order placing thing

--- app.py
+++ app.py
@@ -1,28 +1,77 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-from config import Config
-from model import db
-from routes.user_route import user_bp
-from routes.product_route import product_bp
-from routes.order_route import order_bp
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, FloatField, IntegerField, SubmitField, SelectField
+from wtforms.validators import DataRequired, NumberRange
 
-app = Flask(__name__)
-app.config.from_object(Config)
-CORS(app)
+app = Flask(__name__,template_folder="templates")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
+app.config['SECRET_KEY'] = 'secretkey'
+db = SQLAlchemy(app)
 
-db.init_app(app)
+class ProductForm(FlaskForm):
+    name = StringField('Product Name', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    price = FloatField('Price', validators=[DataRequired(), NumberRange(min=0)])
+    category = StringField('Category')
+    stock = IntegerField('Stock', validators=[DataRequired(), NumberRange(min=0)])
+    submit = SubmitField('Add Product')
 
-# Register Blueprints
-app.register_blueprint(user_bp, url_prefix="/user")
-app.register_blueprint(product_bp, url_prefix="/product")
-app.register_blueprint(order_bp, url_prefix="/order")
+class OrderForm(FlaskForm):
+    product_id = SelectField('Product', coerce=int, validators=[DataRequired()])
+    quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=1)])
+    submit = SubmitField('Place Order')
 
-@app.route("/", methods=["GET"])
-def home():
-    return jsonify({"message": "Flask-SQLite E-commerce API is running!"})
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100))
+    stock = db.Column(db.Integer, default=0)
 
-if __name__ == "__main__":
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = ProductForm()
+    if form.validate_on_submit():
+        product = Product(
+            name=form.name.data,
+            description=form.description.data,
+            price=form.price.data,
+            category=form.category.data,
+            stock=form.stock.data
+        )
+        db.session.add(product)
+        db.session.commit()
+        flash('Product added successfully!', 'success')
+        return redirect(url_for('index'))
+    products = Product.query.all()
+    return render_template('index.html', form=form, products=products)
+
+@app.route('/order', methods=['GET', 'POST'])
+def order():
+    form = OrderForm()
+    form.product_id.choices = [(p.id, p.name) for p in Product.query.all()]
+    if form.validate_on_submit():
+        product = Product.query.get(form.product_id.data)
+        if product and product.stock >= form.quantity.data:
+            product.stock -= form.quantity.data
+            order = Order(product_id=form.product_id.data, quantity=form.quantity.data)
+            db.session.add(order)
+            db.session.commit()
+            flash('Order placed successfully!', 'success')
+        else:
+            flash('Not enough stock!', 'danger')
+        return redirect(url_for('order'))
+    return render_template('order.html', form=form)
+
+if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Creates tables if they don't exist
+        db.create_all()
     app.run(debug=True)
+
--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_NAME = "ecommerce.db"
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, DB_NAME)}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
