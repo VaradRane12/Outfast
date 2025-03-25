1f963c8104eb8bfdb9d9891060803bc748c71469
VaradRane12 <varadjrane@gmail.com>
2025-03-25 10:40:45 +0530

    changed navbar and added banners on home page

--- home.py
+++ home.py
@@ -7,4 +7,9 @@ home_bp = Blueprint('home', __name__)
 @home_bp.route('/', methods=['GET', 'POST'])
 def index():
     random_products = Product.query.order_by(func.random()).limit(9).all()
-    return render_template('index.html', products=random_products)
+    offers = [
+    {"image_link": "static/assets/sale_banner1.jpg"},
+    {"image_link": "static/assets/banner2.jpeg"},
+    {"image_link": "static/assets/banner3.jpeg"},
+]
+    return render_template('index.html', products=random_products,offers=offers)
